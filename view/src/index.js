d8a3c83d7bcdbab3ad75ab9c5ab3eb41a61f8431
uchida189 <perukipoo@icloud.com>
2025-03-21 16:41:20 +0900

    [feat]陣地を作成

--- view/src/index.js
+++ view/src/index.js
@@ -1,10 +1,9 @@
 import { init, Sprite, GameLoop, initKeys, keyPressed } from '../../node_modules/kontra/kontra.mjs';
-// import { init, Sprite, GameLoop } from 'kontra';
 
 let { canvas } = init();  // canvasを初期化
-console.log(canvas.width);
 initKeys();  // キーボード入力を初期化
 
+// プレイヤー
 let player = Sprite({
   x: 100,        // x座標
   y: 80,         // y座標
@@ -14,8 +13,19 @@ let player = Sprite({
   // dx: 2          // x座標の増加量
 });
 
+// 陣地
+let base = Sprite({
+  x: 0,
+  y: 0,
+  color: 'gray',
+  width: 300,
+  height: canvas.height,
+  health: 1200    // 耐久値
+});
+
 let loop = GameLoop({  // ゲームループ
   update: function() {
+    // プレイヤーの操作
     if((keyPressed('arrowup') || keyPressed('w')) && player.y > 0) {
       console.log('up');
       player.y -= 2;
@@ -23,13 +33,16 @@ let loop = GameLoop({  // ゲームループ
       console.log('down');
       player.y += 2;
     }
+    base.update();
     player.update();  // スプライトを更新
+    
     
     // if (player.x > canvas.width) {
     //   player.x = -player.width;
     // }
   },
   render: function() {
+    base.render();
     player.render();  // スプライトを描画
   }
 });
