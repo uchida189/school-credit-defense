bb322912e3ae5fa28cea0cbebb400c63cdb9ba1b
uchida189 <perukipoo@icloud.com>
2025-04-10 15:36:54 +0900

    [fix]画像の読み込み処理をimageAssetsに修正

--- view/src/player.js
+++ view/src/player.js
@@ -1,4 +1,4 @@
-import { Sprite } from '../../node_modules/kontra/kontra.mjs';
+import { Sprite, imageAssets } from '../../node_modules/kontra/kontra.mjs';
 import { PLAYER_TYPE_SETTINGS } from './constants.js';
 
 export class Player {
@@ -9,13 +9,11 @@ export class Player {
         const settings = PLAYER_TYPE_SETTINGS[playerType]; // 定数からプレイヤーのタイプ別設定を取得
         const playerWidth = 120;
         const playerHeight = 20;
-        let image = new Image(playerWidth, playerHeight);
-        image.src = `assets/player${playerType}.png`; // プレイヤーの画像を取得
 
         this.sprite = Sprite({
           x: 100,    // 陣地内からスタート
           y: canvas.height / 2,   // 画面中央
-          image: image,
+          image: imageAssets[`assets/player${playerType}.png`], // プレイヤーの画像を取得
           width: playerWidth,  // プレイヤーの幅
           height: playerHeight, // プレイヤーの高さ
           anchor: { x: 0.5, y: 0.5 },   // 中心を基準にする
@@ -26,6 +24,8 @@ export class Player {
           timeSinceLastFire: 0,       // 最後に弾丸を発射してからの経過時間
           items: [0, 0, 0],           // アイテムの所持数
         });
+        this.sprite.image.width = playerWidth;  // プレイヤーの幅
+        this.sprite.image.height = playerHeight; // プレイヤーの高さ
         console.log('Player created:', this.sprite);
     }
 
